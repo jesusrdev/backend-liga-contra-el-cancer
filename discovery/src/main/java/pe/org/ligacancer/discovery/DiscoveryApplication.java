8cb7bb4e96c5f3ec99497859cdfb46a74c404806
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-09 12:07:38 -0500

    adding configuration to all services and modifying the routes of gateway

--- discovery/src/main/java/pe/org/ligacancer/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/pe/org/ligacancer/discovery/DiscoveryApplication.java
@@ -2,7 +2,9 @@ package pe.org.ligacancer.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
